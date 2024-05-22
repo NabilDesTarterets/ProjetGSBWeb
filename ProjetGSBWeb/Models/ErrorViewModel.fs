a60c7cb21426ad71d55012b0c993519f4bb9fa6a
NabilDesTarterets <jalil.haddar@lmdsio.fr>
2024-05-22 20:02:17 +0200

    Ajoutez des fichiers projet.

---
+++ ProjetGSBWeb/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace ProjetGSBWeb.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
